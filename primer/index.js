e234bcd6681cf467d1944908317e8384abba35bd
Nelson Ripoll <nripoll@fbdfrozen.com>
2021-05-10 11:52:24 -0500

    add code for primer

---
+++ primer/index.js
@@ -0,0 +1,23 @@
+let hatPrice = 100;
+console.log(`Hat price: ${hatPrice}`);
+
+let bootsPrice = "100";
+console.log(`Boots price: ${bootsPrice}`);
+
+if (hatPrice == bootsPrice) { 
+  console.log("Prices are the same");
+} else {
+  console.log("Prices are different");
+}
+
+let totalPrice = hatPrice + bootsPrice;
+console.log(`Total Price: ${totalPrice}`);
+
+let myVariable = "Nelson";
+console.log(`Type of myVariable: ${typeof myVariable}`);
+
+myVariable = 100;
+console.log(`Type of myVariable: ${typeof myVariable}`);
+
+myVariable = null;
+console.log(`Type of myVariable: ${typeof myVariable}`);
